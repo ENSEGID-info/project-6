1ad0b1a25653a1c504a5b0918ee73dd4db9d1546
louprds <louiseprades07@gmail.com>
2025-11-26 09:58:20 +0100

    erosion mnt topotoolbox
    il faut importer le mnt dans topotoolbox

--- etape3/etape3.py
+++ etape3/etape3.py
@@ -1,6 +1,27 @@
+import os
+os.system("pip install --upgrade topotoolbox")
 import topotoolbox
-def etape3_main(result):
-    print("Exécution de l'étape 3...")
-    # Exemple : display of the result
-    print("Maximum is " + str(result) + ".")
+import rasterio
+import matplotlib.pyplot as plt
 
+
+# Charger ton fichier local
+with rasterio.open("MNT.tif") as src:
+    dem = src.read(1)
+    plt.imshow(dem, cmap='terrain')
+    plt.colorbar(label="Altitude (m)")
+    plt.title("Modèle Numérique de Terrain (MNT)")
+    plt.show()
+    
+dem = topotoolbox.load_dem('bigtujunga')
+
+
+eroded = dem.erode((3,3))
+dem.plot(cmap='terrain')
+
+plt.figure()
+eroded.plot(cmap='terrain')
+
+diff=dem-eroded
+plt.figure()
+diff.plot(cmap='terrain')
